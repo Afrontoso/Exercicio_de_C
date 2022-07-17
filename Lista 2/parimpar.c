37ee292c4bdcd07f79070164ca3521e4a10eedd1
Afrontoso <victor2lra@gmail.com>
2022-07-17 20:28:36 -0300

    reformula execicio 3

--- Lista 2/parimpar.c
+++ Lista 2/parimpar.c
@@ -1,122 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    int escolha_A, j1a, j2b,
-        escolha_C, j1c, j2d,
-        escolha_X, j1x, j2y,
-        parimpar;
-    char ganhoab, ganhocd, ganhoxy;
+int main() {
+    int e, j1, j2;
+    char jogada1, jogada2, jogada3;
 
-    scanf("%d %d %d\n", &escolha_A, &j1a, &j2b);
-    scanf("%d %d %d\n", &escolha_C, &j1c, &j2d);
-    scanf("%d %d %d\n", &escolha_X, &j1x, &j2y);
+    scanf("%d %d %d", &e, &j1, &j2);
+    jogada1 = (j1 + j2) % 2 == e ? 'A' : 'B';
+    scanf("%d %d %d", &e, &j1, &j2);
+    jogada2 = (j1 + j2) % 2 == e ? 'C' : 'D';
+    scanf("%d %d %d", &e, &j1, &j2);
+    jogada3 = (j1 + j2) % 2 == e ? jogada1 : jogada2;
 
-    parimpar = j1a + j2b;
-    if (escolha_A == 0){
-        parimpar %= 2;
-        if (parimpar == escolha_A){
-            ganhoab = 'A';
-        }else if (parimpar != escolha_A){
-            ganhoab = 'B';
-        }
-    }
-    else if (escolha_A == 1){
-        parimpar %= 2;
-        if (parimpar == escolha_A){
-            ganhoab = 'A';
-        }
-        else if (parimpar != escolha_A){
-            ganhoab = 'B';
-        }
-    }
-    parimpar = j1c + j2d;
-    if (escolha_C == 0){
-        parimpar %= 2;
-        if (parimpar == escolha_A){
-            ganhocd = 'C';
-        }
-        else if (parimpar != escolha_A){
-            ganhocd = 'D';
-        }
-    }else if (escolha_C == 1){
-        parimpar %= 2;
-        if (parimpar == escolha_A){
-            ganhocd = 'C';
-        }else if (parimpar != escolha_A){
-            ganhocd = 'D';
-        }
-    }
+    printf("%c\n", jogada3);
 
-    if (ganhoab == 'A' && ganhocd == 'C'){
-        parimpar = j1x + j2y;
-        if (escolha_X == 0){
-            parimpar %= 2;
-            if (parimpar == escolha_X){
-                printf("%c", ganhoab);
-            }else if (parimpar != escolha_X){
-                printf("%c", ganhocd);
-            }
-        }else if (escolha_X == 1){
-            parimpar %= 2;
-            if (parimpar == escolha_X){
-                printf("%c", ganhoab);
-            }else if (parimpar != escolha_X){
-                printf("%c", ganhocd);
-            }
-        }
-    }else if (ganhoab == 'A' && ganhocd == 'D'){
-        parimpar = j1x + j2y;
-        if (escolha_X == 0){
-            parimpar %= 2;
-            if (parimpar == escolha_X){
-                printf("%c", ganhoab);
-            }else if (parimpar != escolha_X){
-                printf("%c", ganhocd);
-            }
-        }else if (escolha_X == 1){
-            parimpar %= 2;
-            if (parimpar == escolha_X){
-                printf("%c", ganhoab);
-            }else if (parimpar != escolha_X){
-                printf("%c", ganhocd);
-            }
-        }
-    }else if (ganhoab == 'B' && ganhocd == 'C'){
-        parimpar = j1x + j2y;
-        if (escolha_X == 0){
-            parimpar %= 2;
-            if (parimpar == escolha_X){
-                printf("%c", ganhoab);
-            }else if (parimpar != escolha_X){
-                printf("%c", ganhocd);
-            }
-        }else if (escolha_X == 1){
-            parimpar %= 2;
-            if (parimpar == escolha_X){
-                printf("%c", ganhoab);
-            }else if (parimpar != escolha_X){
-                printf("%c", ganhocd);
-            }
-        }
-    }else if (ganhoab == 'B' && ganhocd == 'D'){
-        parimpar = j1x + j2y;
-        if (escolha_X == 0){
-            parimpar %= 2;
-            if (parimpar == escolha_X){
-                printf("%c", ganhoab);
-            }else if (parimpar != escolha_X){
-                printf("%c", ganhocd);
-            }
-        }else if (escolha_X == 1){
-            parimpar %= 2;
-            if (parimpar == escolha_X){
-                printf("%c", ganhoab);
-            }else if (parimpar != escolha_X){
-                printf("%c", ganhocd);
-            }
-        }
-    }
     return 0;
-}
+}   
